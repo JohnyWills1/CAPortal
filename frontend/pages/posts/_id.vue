c085c393dc128c177af915b219bf44d63fa55929
Johny <jw596@sussex.ac.uk>
2020-05-18 04:55:11 +0100

    6th commit - added comments (very basic functionality)

--- frontend/pages/posts/_id.vue
+++ frontend/pages/posts/_id.vue
@@ -1,41 +1,37 @@
 <template>
-
 <div class="uk-section">
   <div class="uk-container uk-container-small">
 
-  <article class="uk-article">
+    <!-- Post section -->
+    <article class="uk-article">
+      <h1 class="uk-article-title"><a class="uk-link-reset" href="">{{post.title}}</a></h1>
+      <p class="uk-article-meta" v-if="post.user">Written by {{ post.user.username }} on {{moment(post.created_at).format("LL")}}.</p>
+      <p class="uk-article-meta">Likes: <p>{{ post.like_count}}</p><span uk-icon="icon: heart" class="uk-padding-small uk-padding-remove-left"></span></p>
+      <p v-if="post.content" id="editor" v-html="$md.render(post.content)"></p>
+      <div class="uk-grid-small uk-child-width-auto" uk-grid>
+          <div>
+              <a class="uk-button uk-button-text" href="#" v-if="post.comments">{{post.comments.length}} Comments</a>
+          </div>
+      </div>
+    </article>
 
-    <h1 class="uk-article-title"><a class="uk-link-reset" href="">{{post.title}}</a></h1>
-
-    <p class="uk-article-meta" v-if="post.user">Written by {{ post.user.username }} on {{moment(post.created_at).format("LL")}}. Posted in {{post.thread.name}}</p>
-
-    <p class="uk-article-meta">Likes: {{post.like_count}}<span uk-icon="icon: heart" class="uk-padding-small uk-padding-remove-left"></span></p>
-
-    <p v-if="post.content" id="editor" v-html="$md.render(post.content)"></p>
-
-    <div class="uk-grid-small uk-child-width-auto" uk-grid>
-        <div>
-            <a class="uk-button uk-button-text" href="#" v-if="post.comments">{{post.comments.length}} Comments</a>
+    <!-- Comment section -->
+    <div class="uk-section-muted">
+      <div class="uk-container">
+        <div v-for="comment in post.comments">
+          <br>
+          <header class="uk-comment-header">
+            <ul class="uk-comment-meta uk-subnav">
+              <li> {{ comment.like_count }} </li>
+              <li> {{ moment(comment.created_at).format("LLL") }} </li>
+            </ul>
+          </header>
+          <div class="uk-comment-body">{{ comment.content }}</div>
+          <br>
         </div>
-    </div>
-
-</article>
-  <!-- Comment section -->
-  <!-- <div class="uk-section-muted">
-    <div class="uk-container">
-      <div v-for="comment in post.comments">
-        <br>
-        <header class="uk-comment-header">
-          <ul class="uk-comment-meta uk-subnav">
-            <li> {{ comment.like_count }} </li>
-            <li> {{ moment(comment.created_at).format("LLL") }} </li>
-          </ul>
-        </header>
-        <div class="uk-comment-body">{{ comment.content }}</div>
-        <br>
       </div>
     </div>
-  </div> -->
+
   </div>
 </div>
 </template>
