d801e8126910b7cc9aa812afb646e7dc45626817
Johny <jw596@sussex.ac.uk>
2020-05-21 06:34:56 +0100

    tiny spelling error

--- frontend/pages/user/_id.vue
+++ frontend/pages/user/_id.vue
@@ -3,7 +3,7 @@
     <div class="uk-container uk-container-small uk-position-relative uk-text-center">
       <h1 class="uk-heading-large uk-text-center">{{ $auth.user.username }}</h1>
       <hr class="uk-margin-medium">
-      <div v-if="$auth.user.progfilePicture">
+      <div v-if="$auth.user.profilePicture">
         <img :src="api_url + $auth.user.profilePicture.url" style="border-radius: 50%;" width="100" height="100">
       </div>
       <button class="uk-button uk-button-secondary">Change Picture</button>
